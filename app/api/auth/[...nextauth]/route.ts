fb954dfe5e2d06c6ebb73415810ca8b99cc761c1
Nikhil-python7667 <techkinha553@gmail.com>
2026-01-31 21:35:46 +0530

    changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -15,9 +15,33 @@ const handler = NextAuth({
 
   callbacks: {
     async redirect({ baseUrl }) {
-      return `${baseUrl}/dashboard`;
+      return `${baseUrl}/dashboard/page.tsx`;
     },
   },
 });
 
 export { handler as GET, handler as POST };
+
+// import NextAuth, { NextAuthOptions } from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
+
+// export const authOptions: NextAuthOptions = {
+//   debug: true, 
+
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID as string,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+//     }),
+//   ],
+
+//   pages: {
+//     signIn: "/github-login",
+//   },
+
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
