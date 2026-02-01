8cfa6ce31344227ef9eb139044ac80686453e2ae
Nikhil-python7667 <techkinha553@gmail.com>
2026-02-01 23:45:00 +0530

    router changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,18 +3,13 @@ import GithubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
-
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
-
-  pages: {
-    signIn: "/github-login",
-  },
-
+  pages: { signIn: "/github-login" },
   callbacks: {
     async redirect({ baseUrl }) {
       return `${baseUrl}/dashboard`;
@@ -23,6 +18,33 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
+
+
+// import NextAuth from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
+
+// const handler = NextAuth({
+//   secret: process.env.NEXTAUTH_SECRET,
+
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_CLIENT_ID!,
+//       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+//     }),
+//   ],
+
+//   pages: {
+//     signIn: "/github-login",
+//   },
+
+//   callbacks: {
+//     async redirect({ baseUrl }) {
+//       return `${baseUrl}/dashboard`;
+//     },
+//   },
+// });
+
+// export { handler as GET, handler as POST };
 
 // import NextAuth, { NextAuthOptions } from "next-auth";
 // import GithubProvider from "next-auth/providers/github";
